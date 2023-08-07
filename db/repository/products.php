d3b8d72e25926adeb8c55e96dd84f6b35b45e55f
khoile99 <khoi.le99@hcmut.edu.vn>
2023-08-07 23:43:09 +0700

    sort list_products

--- db/repository/products.php
+++ db/repository/products.php
@@ -9,7 +9,7 @@ class Products
     public static function list_products()
     {
         global $dbConnection;
-        $dql = "SELECT id, name, description, brand, color, material, size FROM shop.products";
+        $dql = "SELECT id, name, description, brand, color, material, size FROM shop.products ORDER BY name ASC";
         $records = $dbConnection->query($dql)->fetchAll(\PDO::FETCH_OBJ);
         if (!$records) {
             return null;
